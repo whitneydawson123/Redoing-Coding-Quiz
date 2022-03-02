3b377e6f1ff8e2b7c83561b8bce6fa03cd123411
Whitney Dawson <whitneydawson123@gmail.con>
2022-03-02 14:17:56 -0500

    updated

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,1 +1,34 @@
-// 
+// inner HTML variables
+var startingPage = document.getElementByClassName('.starting');
+var startBtn = document.getElementByClassName('.startBtn');
+var nextBtn = document.getElementByClassName('.nextBtn');
+var endingPage = document.getElementByClassName('.ending');
+var showScore = document.getElementByClassName('.score');
+var enterName = document.getElementByClassName('.name');
+var saveScoreBtn = document.getElementByClassName('.saveScoreBtn');
+
+// start button function
+startBtn.addEventListener('click', function() {
+    
+});
+
+
+// timer and penalize time if there are wrong questions
+
+
+// quiz questions
+
+
+// scoring the questions
+
+
+// name enter
+
+
+// localized storage
+
+
+// seeing high scores
+
+
+// reset game
